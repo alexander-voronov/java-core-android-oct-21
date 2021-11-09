8daafd2aa103186f9764f9aec617b4e62a0abbcc
Alexander Voronov <dr.strum@gmail.com>
2021-11-09 17:38:58 +0300

    HW5_09/11/2021

---
+++ src/main/java/ru/geekbrains/jca/lessons/l5/Main.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.jca.lessons.l5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person[] persArray = new Person[5];
+        persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312311", 30000, 30);
+        persArray[1] = new Person("Petrov Petr", "Assistant", "petrov@mailbox.com", "892312312", 25000, 25);
+        persArray[2] = new Person("Pushkin Alexander", "Boss", "pushkin@mailbox.com", "892312313", 50000, 50);
+        persArray[3] = new Person("Sidorov Sidr", "Accountant", "sidorov@mailbox.com", "892312314", 40000, 45);
+
+        for (int i = 0; i < 4; i++) {
+            if (persArray[i].getAge() > 40) {
+                persArray[i].print();
+            }
+        }
+    }
+}
