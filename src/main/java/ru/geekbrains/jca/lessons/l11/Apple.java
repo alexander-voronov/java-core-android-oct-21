08b346578b9950e03a5e1320a135a6e3bc0ee2a7
Alexander Voronov <dr.strum@gmail.com>
2021-12-01 21:47:19 +0300

    HW11_01/12/2021

---
+++ src/main/java/ru/geekbrains/jca/lessons/l11/Apple.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.jca.lessons.l11;
+
+public class Apple extends Fruit {
+    public Apple() {
+        super(1.0f);
+    }
+}
---
+++ src/main/java/ru/geekbrains/jca/lessons/l11/Orange.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.jca.lessons.l11;
+
+public class Orange extends Fruit {
+    public Orange() {
+        super(1.5f);
+    }
+}
